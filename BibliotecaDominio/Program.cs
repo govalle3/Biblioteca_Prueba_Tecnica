a8135a081af55393ad7d72c3ef19911dc5ee7e5e
German Ovalle Silva <39679249+govalle3@users.noreply.github.com>
2020-10-12 15:31:27 -0500

    Add files via upload

---
+++ BibliotecaDominio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDominio
+{
+    class Program
+    {
+        static void Main(string[] args) // Se debe dejar para evitar errores de compilacion
+        {
+        }
+    }
+}
---
+++ BibliotecaDominio/UtilidadFechas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BibliotecaDominio;
+
+namespace BibliotecaDominio
+{
+    class UtilidadFechas
+    {
+        private UtilidadFechas() { }
+
+        public static DateTime ConvertirToDate(DateTime localDate)
+        {
+            return DateTime.Today;
+        }
+
+        public static DateTime AgregarDias(DateTime fechaSolicitud, int cantidadMaxima)
+        {
+            if (cantidadMaxima < 1)
+            {
+                return fechaSolicitud;
+            }
+
+            DateTime fechaIterador = fechaSolicitud;
+            int diasAgregados = 1;
+            while (diasAgregados < cantidadMaxima)
+            {
+                fechaIterador = fechaIterador.AddDays(1);
+                if (!(fechaIterador.DayOfWeek == DayOfWeek.Sunday))
+                {
+                    ++diasAgregados;
+                }
+            }
+
+            return fechaIterador;
+        }
+    }
+}
